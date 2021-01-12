db95ce37e037426c8497b9908c49c87c2f78b583
Vincent Wilson <vince@classyh.at>
2021-01-12 08:44:08 -0500

    Delete sequelize.js

--- packages/apollos-data-connector-postgres/__mocks__/sequelize.js
+++
@@ -1,8 +0,0 @@
-// import SequelizeMock from 'sequelize-mock';
-
-const sequelizeReal = jest.requireActual('sequelize');
-
-module.exports = {
-  ...sequelizeReal,
-  // Sequelize: SequelizeMock,
-};
