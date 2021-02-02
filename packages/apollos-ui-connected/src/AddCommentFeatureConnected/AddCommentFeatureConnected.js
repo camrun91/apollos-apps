e1a8bb8d70a704aba42a879ff48d0b0b23835bdc
Vincent Wilson <vince@classyh.at>
2021-02-02 13:46:16 -0500

    Added an update function

--- packages/apollos-ui-connected/src/AddCommentFeatureConnected/AddCommentFeatureConnected.js
+++ packages/apollos-ui-connected/src/AddCommentFeatureConnected/AddCommentFeatureConnected.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import PropTypes from 'prop-types';
 import { AddCommentInput } from '@apollosproject/ui-kit';
+
+import GET_NODE_FEATURE_FEED from '../FeaturesFeedConnected/getFeatureFeed';
+import GET_NODE_FEATURES from '../NodeFeaturesConnected/getNodeFeatures';
+import GET_COMMENT_LIST_FEATURE from '../CommentListFeatureConnected/getCommentListFeature';
+
 import GET_ADD_COMMENT_FEATURE from './getAddCommentFeature';
 import ADD_COMMENT from './addComment';
 
@@ -21,9 +26,46 @@ const AddCommentFeatureConnected = ({
   if (featureId && refetch && refetchRef)
     refetchRef({ refetch, id: featureId });
 
-  const [addComment] = useMutation(ADD_COMMENT);
   const currentPerson = data?.currentUser;
   const node = data?.node;
+
+  const [addComment] = useMutation(ADD_COMMENT, {
+    update: (cache, { data: { addComment } }) => {
+      try {
+        const parentNode = cache.readQuery({
+          query: GET_NODE_FEATURES,
+          variables: { nodeId: node.relatedNode.id },
+        }).node;
+        const myFeatureFeed = cache.readQuery({
+          query: GET_NODE_FEATURE_FEED,
+          variables: { featureFeedId: parentNode.featureFeed.id },
+        }).node;
+
+        const commentListFeatureId = myFeatureFeed.features.find(
+          ({ __typename }) => __typename === 'CommentListFeature'
+        ).id;
+
+        const commentListFeature = cache.readQuery({
+          query: GET_COMMENT_LIST_FEATURE,
+          variables: { featureId: commentListFeatureId },
+        });
+
+        cache.writeQuery({
+          query: GET_COMMENT_LIST_FEATURE,
+          variables: { featureId: commentListFeatureId },
+          data: {
+            ...commentListFeature,
+            node: {
+              ...commentListFeature.node,
+              comments: [addComment, ...commentListFeature.node.comments],
+            },
+          },
+        });
+      } catch (e) {
+        console.warn('Failed to update cache after adding comment', e);
+      }
+    },
+  });
 
   return (
     <Component
