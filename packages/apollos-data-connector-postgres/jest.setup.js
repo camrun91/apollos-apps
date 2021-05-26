797d69186bdcc4fd5995ba037a9e6a058d5bd046
Michael Neeley <micneeley14@gmail.com>
2021-05-26 10:57:54 -0400

    chore: stops assuming local DB
    As a new postgres developer, I have been very confused as to what is happening with our local database setup and configuration. Assuming a specific db called "postgres" has made it difficult. Not to mention a database was being created, even though we are specifying (or worse, not specifying) a URL. This PR attempts to clear up what is actually happening when spinning up a local instance by expecting the developer to create their own db if they are choosing to use our DB functionality, something they should already understand, or be forced to learn about. I'll follow up with simple instructions in the template repo.

--- packages/apollos-data-connector-postgres/jest.setup.js
+++ packages/apollos-data-connector-postgres/jest.setup.js
@@ -1,19 +1,21 @@
 import { Client } from 'pg';
-import { ensureLocalDb } from './src/postgres/local-db';
 import { dbName } from './src/postgres/test-connect';
 
-export default async ({ maxWorkers }) => {
-  const client = new Client({
+const createTestDB = async (client, name) => {
+  await client.query(`DROP DATABASE IF EXISTS ${name};`);
+  await client.query(`CREATE DATABASE ${name};`);
+  const dbTestClient = new Client({
     host: 'localhost',
-    database: 'postgres',
+    database: name,
   });
+  await dbTestClient.connect();
+  await dbTestClient.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+  await dbTestClient.end();
+};
 
-  try {
-    await client.connect();
-  } catch (e) {
-    console.error('Failed to connect to local postgres instance');
-    console.error(e);
-  }
+export default async ({ maxWorkers }) => {
+  const client = new Client();
+  await client.connect();
 
   let count = 1;
 
@@ -21,7 +23,7 @@ export default async ({ maxWorkers }) => {
     const name = dbName(count);
 
     // eslint-disable-next-line no-await-in-loop
-    await ensureLocalDb(client, name, true);
+    await createTestDB(client, name);
 
     count += 1;
   }
